e38e21fef0c341d9a7711d1396b92c31e81a9f3d
shreyas-chougale <shreyaschougale311@gmail.com>
2026-07-16 18:54:12 +0530

    Delete lib directory

--- lib/db/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./conversations";
-export * from "./messages";
-export * from "./interview_sessions";
-export * from "./aptitude_sessions";
-export * from "./resume_sessions";
-export * from "./auth";
